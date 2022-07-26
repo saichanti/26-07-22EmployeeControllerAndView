70a4957af99ca4bc8557a11649cab58f362aefe8
saichanti <mosurisaikumar@gmail.com>
2022-07-26 23:37:47 +0530

    Employee controller and view

---
+++ EmployeeControllerAndView Assignment/EmployeeControllerAndView Assignment/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace EmployeeControllerAndView_Assignment.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: EmployeeController
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //Employee Data View
+        public ActionResult EmployeeData()
+        {
+            return View();
+        }
+        public ActionResult Congratulations()
+        {
+            return View();
+        }
+    }  
+}
